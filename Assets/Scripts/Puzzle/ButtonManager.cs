867372684c4c2080b8aa560fd701912e829fd8ac
Berkay Şahin <akassnowflake@gmail.com>
2022-03-26 13:45:12 +0300

    Update ButtonManager.cs

--- Assets/Scripts/Puzzle/ButtonManager.cs
+++ Assets/Scripts/Puzzle/ButtonManager.cs
@@ -7,12 +7,20 @@ using UnityEngine.Events;
 public class ButtonManager : MonoBehaviour
 {
     public List<Buttons> _buttons = new List<Buttons>();
+    public int counter;
+    public int winCounter;
+    public GameObject[] _winObjects;
 
     // private event System.Action<int> OnButtonClicked;
-
+    private void Start()
+    {
+        winCounter = _winObjects.Length;
+    }
     private void Update()
     {
         ControlButtons();
+
+        Debug.Log(winCounter);
     }
 
     private void ControlButtons()
@@ -24,7 +32,7 @@ public class ButtonManager : MonoBehaviour
                 if (_buttons[i].CompareTag("HitButton") && !_buttons[i].buttonState)
                 {
                     Die();
-                } 
+                }
                 if (_buttons[i].CompareTag("NotHitButton") && _buttons[i].buttonState)
                 {
                     Die();
@@ -36,7 +44,17 @@ public class ButtonManager : MonoBehaviour
     {
         foreach (var button in _buttons)
         {
+            Debug.Log("lose aldin");
             button.buttonState = false;
         }
     }
+
+    private void CheckWinStatus()
+    {
+        foreach(var button in _buttons)
+        {
+            
+        }
+    }
+
 }

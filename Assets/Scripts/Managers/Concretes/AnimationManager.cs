0eb868963bb3f0541b3f2600c44f2680b5cc6104
TheMOON <mert__balkan@hotmail.com>
2022-03-27 07:25:14 +0300

    menu door anim

--- Assets/Scripts/Managers/Concretes/AnimationManager.cs
+++ Assets/Scripts/Managers/Concretes/AnimationManager.cs
@@ -7,7 +7,8 @@ namespace ANKU.Animations.Concretes
     
     public class AnimationManager : MonoBehaviour
     {
-        public static AnimationManager Instance { get; private set; }        
+        public static AnimationManager Instance { get; private set; }
+        [SerializeField] private GameObject door;
         private void Awake()
         {
             SingletonObject();
@@ -26,7 +27,7 @@ namespace ANKU.Animations.Concretes
         }
         public void PlayAnimation()
         {
-            
+            door.transform.Rotate(0, 90, 0);
         }
     }
 }
